cf7cbe00bfa34706ea338ad0fa17ebb11173891d
roopesh-jsp <rupzkumar5@gmail.com>
2023-11-01 18:40:41 +0530

    paths ready

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+const pathbox1 = document.querySelector(".pathbox1");
+const divbox = document.querySelector(".div");
+var yellowpath = [];
+for (let i = 1; i <= 50; i++) {
+  yellowpath.push(i);
+}
+console.log(yellowpath);
+var greenpath = [];
+for (let i = 14; i <= 51; i++) {
+  greenpath.push(i);
+}
+for (let i = 0; i <= 11; i++) {
+  greenpath.push(i);
+}
+console.log(greenpath);
+var redpath = [];
+for (let i = 27; i <= 51; i++) {
+  redpath.push(i);
+}
+for (let i = 0; i <= 24; i++) {
+  redpath.push(i);
+}
+console.log(redpath);
+var bluepath = [];
+for (let i = 40; i <= 51; i++) {
+  bluepath.push(i);
+}
+for (let i = 0; i <= 37; i++) {
+  bluepath.push(i);
+}
+console.log(bluepath);
